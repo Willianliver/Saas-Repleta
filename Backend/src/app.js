6c59ae51cb369d0a3fa7f5ac22060b4b95b43be3
will <pilapires@gmail.com>
2026-04-23 11:54:16 -0300

    test type commonjs

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,6 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 // Lê JSON do body das requisições
